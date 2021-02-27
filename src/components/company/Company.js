84c2a0bcc04bbdc81fb6870c91a36b822511aa90
Leonidas <ldbrooks297@gmail.com>
2021-02-26 16:25:30 -0800

    added css to company.js

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,14 +1,18 @@
+import './Company.css' //same level as company component
+
 const Company = (props) => {
     const {student, ceo, employees, name, valuation, founder} = props.company //gains access to company prop set up in app.ks
 
     return(
-        <div>
-            <p>Student: {student}</p>
-            <p>Name: {name}</p>
-            <p>Founder: {founder}</p>
-            <p>Ceo: {ceo}</p>
-            <p>Valuation: {valuation}</p>
-            <p>Employees: {employees}</p>
+        <div className="company">
+            <ul>
+            <li>Student: {student}</li>
+            <li>Name: {name}</li>
+            <li>Founder: {founder}</li>
+            <li>Ceo: {ceo}</li>
+            <li>Valuation: {valuation}</li>
+            <li>Employees: {employees}</li>
+            </ul>
         </div>
     )
 }
