ba224eb823d2b00e1ff43a422145b7b56584433e
mbrown <tugtuj@gmail.com>
2021-02-26 21:11:56 -0500

    Capsule.js-add file

---
+++ src/components/capsules/Capsule.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Capsule.css'
+
+
+const Capsule = (props) => {
+    const { water_landings, last_update, serial, type } = props.capsule;
+    return (
+        <div>
+            <ul>
+                <li>Serial: {serial}</li>
+                <li>Type: {type}</li>
+                <li>Water Landings: {water_landings}</li>
+                <li>Last Update: {last_update}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Capsule;
